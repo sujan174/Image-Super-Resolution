0c7bbd08008125b3ef720445e8bc1712545a5c81
sujan174 <sjn.174@gmail.com>
2025-08-12 14:43:22 +0530

    final

--- models/user.js
+++ models/user.js
@@ -14,6 +14,10 @@ const userSchema = mongoose.Schema({
         type: String,
         required: true,
     },
+    admin:{
+        type:Boolean,
+        default: false
+    },
     imageHistory: [{
         path: {
             type: String, 

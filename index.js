761634072a71003755989da42d7bcebef319a740
Sandesh <sandeshatdiscord@gmail.com>
2025-10-21 15:05:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,30 +1,27 @@
 require('dotenv').config();
 const express = require('express');
 const path = require('path');
-const connectToMongoDB = require("./controllers/utils/connectToMongoDB");
-const cookieParser = require("cookie-parser");
-const logMiddleware = require("./middleware/logs");
+const cookieParser = require('cookie-parser');
+const connectToMongoDB = require('./controllers/utils/connectToMongoDB');
+const logMiddleware = require('./middleware/logs');
+const uploadRouter = require('./routes/upload');
+const userRouter = require('./routes/user');
 
 const app = express();
 const PORT = 8000;
 
-connectToMongoDB("mongodb://127.0.0.1:27017/upscaler");
+connectToMongoDB('mongodb://127.0.0.1:27017/upscaler');
 
-const uploadRouter = require("./routes/upload"); 
-const userRouter = require("./routes/user"); 
-
-app.set("view engine", "ejs");
-app.set("views", "views");
-
-app.use(express.json()); 
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(logMiddleware);
-
-app.use("/upload", uploadRouter);
-app.use("/user", userRouter);
+app.use('/upload', uploadRouter);
+app.use('/user', userRouter);
 
 app.listen(PORT, () => {
-    console.log(`App started on port ${PORT}`);
+  console.log(`App started on port ${PORT}`);
 });

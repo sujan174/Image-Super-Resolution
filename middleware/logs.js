e8e995bdc13548ce18b67388144dc792c8b49dcf
Sandesh <sandeshatdiscord@gmail.com>
2025-10-21 15:09:58 +0530

    Update logs.js

--- middleware/logs.js
+++ middleware/logs.js
@@ -4,15 +4,15 @@ const path = require('path');
 const logFilePath = path.join(__dirname, '../log.txt');
 
 function logMiddleware(req, res, next) {
-    const logEntry = `${new Date().toISOString()} - ${req.method}: ${req.originalUrl}\n`;
+  const logEntry = `${new Date().toISOString()} - ${req.method}: ${req.originalUrl}\n`;
 
-    fs.appendFile(logFilePath, logEntry, (err) => {
-        if (err) {
-            console.error("Failed to write to log file:", err);
-        }
-    });
+  fs.appendFile(logFilePath, logEntry, (err) => {
+    if (err) {
+      console.error('Failed to write to log file:', err);
+    }
+  });
 
-    next();
+  next();
 }
 
 module.exports = logMiddleware;

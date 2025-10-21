b6d50053c692d1190c698dbd13009ae55cb28fe7
Sandesh <sandeshatdiscord@gmail.com>
2025-10-21 15:08:04 +0530

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,33 +1,33 @@
 const mongoose = require('mongoose');
 
 const userSchema = mongoose.Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    password: {
-        type: String,
-        required: false
-    },
-    admin:{
-        type:Boolean,
-        default: false
-    },
-    imageHistory: [{
-        originalPath: { type: String, required: true },
-        upscaledPath: { type: String, required: true },
-        modelUsed: { type: String, required: true },
-        scaleFactor: { type: Number, required: true },
-        likedResult: { type: Boolean, default: null },
-        time: { type: Date, default: Date.now }
-    }]
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: false,
+  },
+  admin: {
+    type: Boolean,
+    default: false,
+  },
+  imageHistory: [{
+    originalPath: { type: String, required: true },
+    upscaledPath: { type: String, required: true },
+    modelUsed: { type: String, required: true },
+    scaleFactor: { type: Number, required: true },
+    likedResult: { type: Boolean, default: null },
+    time: { type: Date, default: Date.now },
+  }],
 }, {
-    timestamps: true 
+  timestamps: true,
 });
 
 const User = mongoose.model('User', userSchema);

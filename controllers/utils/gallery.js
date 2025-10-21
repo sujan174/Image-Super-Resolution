9a598d1d645fb00255ee7af1007f0d6da3723ccc
Sandesh <sandeshatdiscord@gmail.com>
2025-10-21 15:12:00 +0530

    Update gallery.js

--- controllers/utils/gallery.js
+++ controllers/utils/gallery.js
@@ -1,27 +1,26 @@
 const userModel = require('../../models/user');
 
 async function handleGallery(req, res) {
-    try {
-        const user = await userModel.findById(req.user.id).lean();
+  try {
+    const user = await userModel.findById(req.user.id).lean();
 
-        if (!user) {
-            return res.redirect('/user/login');
-        }
-
-        res.render('gallery', {
-            user: user,
-            images: user.imageHistory ? user.imageHistory.reverse() : [],
-            error: null
-        });
-
-    } catch (error) {
-        console.error("Error fetching gallery data:", error);
-        res.render('gallery', {
-            user: { name: 'Guest' },
-            images: [],
-            error: "Could not load your gallery."
-        });
+    if (!user) {
+      return res.redirect('/user/login');
     }
+
+    res.render('gallery', {
+      user,
+      images: user.imageHistory ? user.imageHistory.reverse() : [],
+      error: null,
+    });
+  } catch (error) {
+    console.error('Error fetching gallery data:', error);
+    res.render('gallery', {
+      user: { name: 'Guest' },
+      images: [],
+      error: 'Could not load your gallery.',
+    });
+  }
 }
 
 module.exports = handleGallery;

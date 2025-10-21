60fce323635aad3208fcf91a959db2d7f222561a
Sandesh <sandeshatdiscord@gmail.com>
2025-10-21 15:10:56 +0530

    Update redirectIfLoggedIn.js

--- middleware/redirectIfLoggedIn.js
+++ middleware/redirectIfLoggedIn.js
@@ -1,19 +1,19 @@
-const jwt = require("jsonwebtoken");
+const jwt = require('jsonwebtoken');
 
 function redirectIfLoggedIn(req, res, next) {
-    const token = req.cookies.token;
+  const token = req.cookies.token;
 
-    if (!token) {
-        return next();
-    }
+  if (!token) {
+    return next();
+  }
 
-    try {
-        jwt.verify(token, process.env.JWT_SECRET);
-        return res.redirect("/upload");
-    } catch (error) {
-        res.clearCookie("token");
-        return next();
-    }
+  try {
+    jwt.verify(token, process.env.JWT_SECRET);
+    return res.redirect('/upload');
+  } catch (error) {
+    res.clearCookie('token');
+    return next();
+  }
 }
 
 module.exports = redirectIfLoggedIn;
